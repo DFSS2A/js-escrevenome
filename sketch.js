85d437e35adf56650d2716975cf39544815acb11
DFSS2A <00001106642429sp@al.educacao.sp.gov.br>
2024-08-29 09:21:14 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(500, 500);
+    background("black");
+  }
+  
+  function draw() {
+    
+    stroke("gray");
+    fill("white");
+  
+    // console.log(mouseIsPressed);
+  
+    if (mouseIsPressed) {           
+       rect(mouseX, mouseY, 20, 35);
+    }
+    
+  }
+  
